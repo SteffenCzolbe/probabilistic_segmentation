df6f56c3918b1851fe316474d1932a24c48670ee
Kasra <kasra@di.ku.dk>
2020-10-28 11:08:45 +0100

    added steps

--- src/lightning_models/mc_dropout.py
+++ src/lightning_models/mc_dropout.py
@@ -12,42 +12,38 @@ class MCDropout(pl.LightningModule):
         self.save_hyperparameters(hparms)
 
     def forward(self, x):
-        raise NotImplementedError()
+        return self.unet(x)
 
     def training_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
-        loss = None
-        raise NotImplementedError()
-        self.log('train_loss', loss)
+        loss = F.cross_entropy(y_hat, y[:, 0])
+        self.log("train/loss", loss)
         return loss
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
-        raise NotImplementedError()
-        loss = None
-        self.log('val_loss', loss)
+        loss = F.cross_entropy(y_hat, y[:, 0])
+        self.log("val/loss", loss)
 
     def test_step(self, batch, batch_idx):
         x, y = batch
         y_hat = self(x)
-        loss = None
-        raise NotImplementedError()
-        self.log('test_loss', loss)
+        loss = F.cross_entropy(y_hat, y[:, 0])
+        self.log("test/loss", loss)
 
     def configure_optimizers(self):
+        print("***************", self.parameters())
         return torch.optim.Adam(self.parameters(), lr=self.hparams.learning_rate)
 
     @staticmethod
     def model_name():
-        raise NotImplementedError()
-        return ''
+        return "MC Dropout"
 
     @staticmethod
     def model_shortname():
-        raise NotImplementedError()
-        return ''
+        return "mcdropout"
 
     def pixel_wise_probabaility(self, x, sample_cnt=None):
         """return the pixel-wise probability map
@@ -79,9 +75,20 @@ class MCDropout(pl.LightningModule):
     @staticmethod
     def add_model_specific_args(parent_parser):
         parser = ArgumentParser(
-            parents=[parent_parser], add_help=False, conflict_handler='resolve')
-        parser.add_argument('--num_filters', type=int, nargs='+', default=[
-                            32, 64, 128, 192], help='Number of Channels for the U-Net architecture. Decoder uses the reverse. Default: 32 64 128 192')
-        parser.add_argument('--learning_rate', type=float, default=0.0001)
-        raise NotImplementedError()
+            parents=[parent_parser], add_help=False, conflict_handler="resolve"
+        )
+        parser.add_argument(
+            "--num_filters",
+            type=int,
+            nargs="+",
+            default=[32, 64, 128, 192],
+            help="Number of Channels for the U-Net architecture. Decoder uses the reverse. Default: 32 64 128 192",
+        )
+        parser.add_argument("--learning_rate", type=float, default=0.0001)
+        parser.add_argument(
+            "--dropout_prob",
+            type=float,
+            default=0,
+            help="The probability of setting output to zero.",
+        )
         return parser
