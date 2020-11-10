217aac08bffdfc849675dcc4bf570d5a3d486499
Steffen <per.sc@di.ku.dk>
2020-11-10 13:14:42 +0100

    tuned mcdropout prob by validation loss

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,1 +1,4 @@
+python3 -m src.test --model_path trained_models/softm --gpus 0,1
+python3 -m src.test --model_path trained_models/ensemble --gpus 0,1
 python3 -m src.test --model_path trained_models/mcdropout --gpus 0,1
+python3 -m src.test --model_path trained_models/punet --gpus 0,1
