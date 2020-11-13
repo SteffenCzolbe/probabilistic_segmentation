eb50cb10bdf119d05c39b630adc3c556c45a2611
Steffen <per.sc@di.ku.dk>
2020-11-13 11:09:30 +0100

    prepared for multiple datasets

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,4 +1,4 @@
-python3 -m src.test --model_path trained_models/softm --gpus 0,1
-python3 -m src.test --model_path trained_models/ensemble --gpus 0,1
-python3 -m src.test --model_path trained_models/mcdropout --gpus 0,1
-python3 -m src.test --model_path trained_models/punet --gpus 0,1
+python3 -m src.test --model_path trained_models/lidc/softm --gpus 0,1
+python3 -m src.test --model_path trained_models/lidc/ensemble --gpus 0,1
+python3 -m src.test --model_path trained_models/lidc/mcdropout --gpus 0,1
+python3 -m src.test --model_path trained_models/lidc/punet --gpus 0,1
--- src/test.py
+++ src/test.py
@@ -26,6 +26,7 @@ def cli_main():
     model = util.load_model_from_checkpoint(args.model_path)
     checkpoint_path = util.get_checkpoint_path(args.model_path)
     datamodule = util.load_datamodule_for_model(model)
+    dataset = model.hparams.dataset
 
     # ------------
     # file
@@ -35,25 +36,28 @@ def cli_main():
             test_results = json.load(f)
     else:
         test_results = {}
-    test_results[args.model_path] = {}
-    test_results[args.model_path]['model_name'] = model.model_name()
-    test_results[args.model_path]['model_shortname'] = model.model_shortname()
+
+    if dataset not in test_results:
+        test_results[dataset] = {}
+    test_results[dataset][args.model_path] = {}
+    test_results[dataset][args.model_path]['model_name'] = model.model_name()
+    test_results[dataset][args.model_path]['model_shortname'] = model.model_shortname()
 
     # ------------
     # Run model test script
     # ------------
-    print(model)
     trainer = pl.Trainer.from_argparse_args(args)
     test_metrics = trainer.test(
         model=model, ckpt_path=checkpoint_path, datamodule=datamodule)
     for k, v in test_metrics[0].items():
-        test_results[args.model_path][k] = v
+        test_results[dataset][args.model_path][k] = v
 
     # ------------
     # save results
     # ------------
     print('Test results:')
-    print(json.dumps(test_results[args.model_path], indent=4, sort_keys=True))
+    print(json.dumps(test_results[dataset]
+                     [args.model_path], indent=4, sort_keys=True))
     with open(args.file, 'w') as json_file:
         json.dump(test_results, json_file, indent=4, sort_keys=True)
 
