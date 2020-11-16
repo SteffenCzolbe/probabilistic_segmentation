b139b51e6468607e58a82cd8da9fddd7950abeb6
Kasra <kasra@di.ku.dk>
2020-11-16 18:14:21 +0100

    added sampler for active learning

--- src/datamodels/lidc_datamodule.py
+++ src/datamodels/lidc_datamodule.py
@@ -10,8 +10,7 @@ from .augmentation import RandomAffine
 
 
 class LIDCDataModule(pl.LightningDataModule):
-
-    def __init__(self, data_dir: str = './data/lidc/', batch_size: int = 32):
+    def __init__(self, data_dir: str = "./data/lidc/", batch_size: int = 32):
         """Data module for the LIDC dataset
 
         Args:
@@ -21,33 +20,36 @@ class LIDCDataModule(pl.LightningDataModule):
         super().__init__()
         self.data_dir = data_dir
         self.batch_size = batch_size
+        self.sampler = None
 
         self.dims = (1, 128, 128)
         self.augment = RandomAffine()
-        self.transform = transforms.Compose([
-            transforms.ToTensor(),
-        ])
+        self.transform = transforms.Compose(
+            [
+                transforms.ToTensor(),
+            ]
+        )
 
     def prepare_data(self):
         # check if data available
         if not os.path.isdir(os.path.join(self.data_dir, "LIDC_crops")):
-            print('Data not found. Downloading...')
+            print("Data not found. Downloading...")
             from data.lidc.download import main as download_lidc
+
             download_lidc()
 
     def train_dataloader(self):
         dataset = LIDCDataset(
-            self.data_dir, "train", transform=self.transform, augment=self.augment)
-        return DataLoader(dataset, batch_size=self.batch_size)
+            self.data_dir, "train", transform=self.transform, augment=self.augment
+        )
+        return DataLoader(dataset, batch_size=self.batch_size, sampler=self.sampler)
 
     def val_dataloader(self):
-        dataset = LIDCDataset(
-            self.data_dir, "val", transform=self.transform)
+        dataset = LIDCDataset(self.data_dir, "val", transform=self.transform)
         return DataLoader(dataset, batch_size=self.batch_size)
 
     def test_dataloader(self):
-        dataset = LIDCDataset(
-            self.data_dir, "test", transform=self.transform)
+        dataset = LIDCDataset(self.data_dir, "test", transform=self.transform)
         return DataLoader(dataset, batch_size=self.batch_size)
 
 
@@ -56,10 +58,12 @@ class LIDCDataset(Dataset):
         self.transform = transform
         self.augment = augment
         self.annotation_dir = os.path.join(
-            data_dir, "LIDC_crops", "LIDC_DLCV_version", datasplit, "lesions")
+            data_dir, "LIDC_crops", "LIDC_DLCV_version", datasplit, "lesions"
+        )
         self.annotations = os.listdir(self.annotation_dir)
         self.img_dir = os.path.join(
-            data_dir, "LIDC_crops", "LIDC_DLCV_version", datasplit, "images")
+            data_dir, "LIDC_crops", "LIDC_DLCV_version", datasplit, "images"
+        )
         self.images = os.listdir(self.img_dir)
 
         # biold tuples of X, Y
@@ -75,7 +79,9 @@ class LIDCDataset(Dataset):
 
         # load, augment and transform images
         x, ys = self.data[index]
-        return self.load_and_transform_image(x), [self.load_and_transform_image(y) for y in ys]
+        return self.load_and_transform_image(x), [
+            self.load_and_transform_image(y) for y in ys
+        ]
 
     def load_and_transform_image(self, fname):
         # load image
@@ -104,12 +110,14 @@ class LIDCDataset(Dataset):
             while len(annotations) > 0 and annotations[0].startswith(image_prefix):
                 annotation = annotations.pop(0)
                 matches_for_this_image.append(
-                    os.path.join(self.annotation_dir, annotation))
-            self.data.append((os.path.join(self.img_dir, image),
-                              tuple(matches_for_this_image)))
+                    os.path.join(self.annotation_dir, annotation)
+                )
+            self.data.append(
+                (os.path.join(self.img_dir, image), tuple(matches_for_this_image))
+            )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     dm = LIDCDataModule()
     ds = dm.test_dataloader()
     print(ds.dataset.data[122])
@@ -119,4 +127,5 @@ if __name__ == '__main__':
     print(sample[1][0].shape)
 
     from torchvision.utils import save_image
-    save_image(sample[0], 'test.png')
+
+    save_image(sample[0], "test.png")
