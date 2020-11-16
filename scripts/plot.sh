ca01d5230a246e644ba88c2abeec9b45df010ca4
Steffen <per.sc@di.ku.dk>
2020-11-16 12:09:23 +0100

    dice overlap plots

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -4,6 +4,8 @@ python3 -m src.plot.viz_predictions --model_dir trained_models/lidc --output_fil
 # GED-plot
 python3 -m src.plot.ged_plot --dataset lidc --output_file plots/ged.png plots/ged.pdf
 
+# SoftDice loss on heatmaps plot
+python3 -m src.plot.soft_dice_plot --dataset lidc --output_file plots/soft_dice.png plots/soft_dice.pdf
 
 # Uncertainty - Seg error correlation
 python3 -m src.plot.correlation --dataset lidc --output_file plots/correlation.png plots/correlation.pdf
