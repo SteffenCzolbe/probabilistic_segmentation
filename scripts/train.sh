38c93f4cfda3ea14145d25c7d35a839aef24620e
Steffen <per.sc@di.ku.dk>
2020-12-03 17:10:11 +0100

    new training params

--- scripts/train.sh
+++ scripts/train.sh
@@ -5,5 +5,5 @@ python3 -m src.train --gpus 0,1 --model punet --dataset lidc --num_filters 32 64
 
 python3 -m src.train --gpus 0,1 --model softm --dataset isic18 --num_filters 32 64 128 192 --max_epochs 150 --batch_size 16  --notest
 python3 -m src.train --gpus 0,1 --model ensemble --dataset isic18 --num_filters 32 64 128 192 --max_epochs 150 --num_models 4 --batch_size 16  --notest
-python3 -m src.train --gpus 1 --model mcdropout --dataset isic18 --num_filters 32 64 128 192 --max_epochs 250 --dropout_prob 0.2 --batch_norm --weight_decay 0 --batch_size 16  --notest --early_stop_patience 50
-python3 -m src.train --gpus 0,1 --model punet --dataset isic18 --num_filters 32 64 128 192 --max_epochs 150 --beta 0.0005 --latent_space_dim 6 --dropout --batch_norm --batch_size 16  --notest
+python3 -m src.train --gpus 0,1 --model mcdropout --dataset isic18 --num_filters 32 64 128 192 --max_epochs 700 --dropout_prob 0.5 --learning_rate 0.00001 --weight_decay 0 --batch_size 16  --notest --early_stop_patience 100
+python3 -m src.train --gpus 0,1 --model punet --dataset isic18 --num_filters 32 64 128 192 --max_epochs 1000 --learning_rate 0.00001 --beta 0.0005 --latent_space_dim 6 --dropout --batch_norm --batch_size 16  --notest --early_stop_patience 100
