421fda50129b77e56e6e0e8377b5e006ac5bc94f
kasra-arnavaz <kasra@di.ku.dk>
2020-12-08 10:24:49 +0100

    ready to use now

--- scripts/run_active_test.sh
+++ scripts/run_active_test.sh
@@ -6,5 +6,5 @@ python3 -m src.active_learning --gpus 1 --model punet --dataset lidc --num_filte
 python3 -m src.active_learning --gpus 1 --model softm --dataset isic18 --num_filters 1 --max_epochs 1 --batch_size 16  --start_with 5 --add 5 --num_iters 1
 python3 -m src.active_learning --gpus 1 --model ensemble --dataset isic18 --num_filters 1 --max_epochs 1 --num_models 4 --batch_size 16  --start_with 5 --add 5 --num_iters 1
 python3 -m src.active_learning --gpus 1 --model mcdropout --dataset isic18 --num_filters 1 --max_epochs 1 --dropout_prob 0.5 --learning_rate 0.00001 --weight_decay 0 --batch_size 16  --start_with 5 --add 5 --num_iters 1
-python3 -m src.active_learning --gpus 1 --model punet --dataset isic18 --num_filters 32 64 128 192 --max_epochs 100 --learning_rate 0.00001 --beta 0.0005 --latent_space_dim 6 --dropout --batch_norm --batch_size 16  --start_with 5 --add 5 --num_iters 1
+python3 -m src.active_learning --gpus 1 --model punet --dataset isic18 --num_filters 32 64 128 192 --max_epochs 1 --learning_rate 0.00001 --beta 0.0005 --latent_space_dim 6 --dropout --batch_norm --batch_size 16  --start_with 5 --add 5 --num_iters 1
 
